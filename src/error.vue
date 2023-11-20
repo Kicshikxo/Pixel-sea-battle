d047b6a3a1d6540363c054863c70880da81eb6ca
Kicshikxo <supermegavalera9000@gmail.com>
2023-11-20 17:34:31 +0300

    feat: add icons at error page

--- src/error.vue
+++ src/error.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="error-page">
     <pixel-form class="error-page__form" @submit="handleSubmit">
+      <icon name="pixelarticons:mood-sad" size="64" />
       <div class="error-page__status-code">{{ $t('errorPage.error', { statusCode: error.statusCode }) }}</div>
       <div v-if="error.statusCode === 404" class="error-page__message">{{ $t('errorPage.error404') }}</div>
       <div v-else-if="error.statusCode === 500" class="error-page__message">{{ $t('errorPage.error500') }}</div>
       <div v-else class="error-page__message">{{ error.message }}</div>
-      <pixel-button type="submit" :label="$t('errorPage.goBackHome')"></pixel-button>
+      <pixel-button type="submit" :label="$t('errorPage.goBackHome')">
+        <template #append-icon>
+          <icon name="pixelarticons:home" />
+        </template>
+      </pixel-button>
     </pixel-form>
   </div>
 </template>
@@ -31,6 +36,7 @@ const handleSubmit = () => {
   min-height: 100vh;
   max-height: 100vh;
   overflow: hidden;
+  background: var(--px-color-page-background);
 
   &__form {
     width: 400px !important;
