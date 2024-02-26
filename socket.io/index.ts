7fb52c6e35474d7a3bcbcc73efbdb6a26409c0c9
Kicshikxo <supermegavalera9000@gmail.com>
2024-02-27 00:47:15 +0300

    dev: fix typo

--- socket.io/index.ts
+++ socket.io/index.ts
@@ -1,7 +1,7 @@
 import { Server as HttpServer } from 'node:http'
 import { Server as SocketServer } from 'socket.io'
 import { connectionHandler } from '~/socket.io/server/socket.io/handlers'
-import * as middlewarer from '~/socket.io/server/socket.io/middleware'
+import * as middleware from '~/socket.io/server/socket.io/middleware'
 import type { ClientToServerEvents, ServerToClientEvents } from '~/socket.io/types'
 
 export const io = {} as { server: SocketServer<ClientToServerEvents, ServerToClientEvents> }
@@ -9,7 +9,7 @@ export const io = {} as { server: SocketServer<ClientToServerEvents, ServerToCli
 export default (nuxtServer: HttpServer) => {
   io.server = new SocketServer(nuxtServer)
 
-  Object.values(middlewarer).forEach(({ handler }) => io.server.use(handler))
+  Object.values(middleware).forEach(({ handler }) => io.server.use(handler))
 
   io.server.on(connectionHandler.event, connectionHandler.handler)
 }
